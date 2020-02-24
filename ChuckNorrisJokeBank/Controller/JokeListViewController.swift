a594c4d7e2c9ed455c80ae61d25fa2e524cfcc03
Daniel Sutcliffe <dannysutcliffe@hotmail.com>
2020-02-24 23:37:29 +0000

    Add Table View

--- ChuckNorrisJokeBank/Controller/JokeListViewController.swift
+++ ChuckNorrisJokeBank/Controller/JokeListViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class JokeListViewController: UIViewController {
+    
+    let randomJoke = ["Joke 1", "Joke 2", "Joke 3", "Joke 4", "Joke 5", "Joke 6", "Joke 7", "Joke 8", "Joke 9", "Joke 10"]
+    
+    @IBAction func okButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,8 +22,24 @@ class JokeListViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func okButtonPressed(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+}
+
+extension JokeListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return randomJoke.count
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = randomJoke[indexPath.row]
+        
+//        let joke = randomJoke[indexPath.row]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! NEJokeCell
+//        cell.setJoke(randomJoke: joke)
+        
+        return cell
     }
     
 }
